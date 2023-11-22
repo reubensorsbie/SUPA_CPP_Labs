d37596a2fc36d8277feeadf8c4a7670d0da3e1ec
reubensorsbie <118668786+reubensorsbie@users.noreply.github.com>
2023-11-22 16:10:00 +0000

    Week2 1

--- Exercises2023/Ex1_2/CustomFunctions.cxx
+++ Exercises2023/Ex1_2/CustomFunctions.cxx
@@ -4,7 +4,40 @@
 #include<vector>
 #include<cmath>
 
-std::vector<std::vector<float>> read_data(std::string filename){
+// overloaded print functions
+void print_result(std::string result){
+
+    std::printf("'%s'\n", result.c_str());
+}
+
+
+void print_result(double result){
+
+    std::printf("%f", result);
+}
+
+
+void print_result(std::vector<double> result){
+    
+    for (int i=0; i < result.size(); ++i){
+        std::printf("%f\n", result[i]);
+    }
+}
+
+
+void print_result(std::vector<std::vector<double>> result){
+
+    for (int i = 0; i < result.size(); ++i){
+        for (int j = 0; j < result[i].size(); ++j){
+
+            std::printf("%f ", result[i][j]);
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+std::vector<std::vector<double>> read_data(std::string filename){
     // INPUT: filename - file name as string
     // OUTPUT: raw_data - vector of vectors containing coordinate pairs as floats
     //         from data, first line skipped
@@ -17,13 +50,13 @@ std::vector<std::vector<float>> read_data(std::string filename){
     if (fileReader.fail()){
 
         // if fail, print error in terminal and exit
-        std::cout << "File Opening Failed" << std::endl;
+        print_result("File Opening Failed");
         exit(1);
     }
     else {
 
         // if success, print message to terminal
-        std::cout << "File Opened Successfully" << std::endl;
+        print_result("File Opened Successfully");
     }
     
     // **READ DATA**
@@ -31,7 +64,7 @@ std::vector<std::vector<float>> read_data(std::string filename){
     // prepare string for parsing each line of file
     std::string line;
     //prepare data structure for storage
-    std::vector<std::vector<float>> raw_data;
+    std::vector<std::vector<double>> raw_data;
 
     // skip the first line (more efficient than checking the data type each call)
     std::getline(fileReader, line);
@@ -40,13 +73,13 @@ std::vector<std::vector<float>> read_data(std::string filename){
     while(std::getline(fileReader, line)){
 
         // create vector to store coordinate pairs
-        std::vector<float> row;
+        std::vector<double> row;
         
         // treat line as stream
         std::stringstream ss(line);
         std::string cell;
 
-        // convert cell data from string to float and add to row to give [x,y] data pairs
+        // convert cell data from string to double and add to row to give [x,y] data pairs
         while(std::getline(ss,cell,',')){
 
             // add each coordinate to intermediate vector
@@ -61,7 +94,8 @@ std::vector<std::vector<float>> read_data(std::string filename){
     return raw_data;
 }
 
-void print_data(std::vector<std::vector<float>> data){
+
+void print_data(std::vector<std::vector<double>> data){
     // INPUTS:
     //  *   data - data in the form of a vector of vectors containing coordinate pairs 
     //      as floats from data, first line skipped
@@ -73,25 +107,30 @@ void print_data(std::vector<std::vector<float>> data){
     std:: cin >> n;
     
     if (n > data.size()){
-        std::cout << "Invalid Number of Lines: Number of lines is larger than data file" << std::endl;
+        print_result("Invalid Number of Lines: Number of lines is larger than data file");
         n = 5;
     }
 
     // Title data:
     std::cout << std::endl << "x,y Data:" << std::endl << std::endl;
 
-    for (int i=0; i<n; ++i){
+    // define subset of data to print (the first n lines)
+    std::vector<std::vector<double>> subset = {data.begin(), data.begin() + n};
+    print_result(subset);
 
-        for (int j=0; j<data[i].size(); ++j){
+    // for (int i=0; i<n; ++i){
 
-            std::cout << data[i][j] << " ";
-        }
+    //     for (int j=0; j<data[i].size(); ++j){
+
+    //         std::cout << data[i][j] << " ";
+    //     }
         
-        std::cout << std::endl;
-    }
+    //     std::cout << std::endl << std::endl;
+    // }
 }
 
-double get_magnitude(std::vector<float> coords){
+
+double get_magnitude(std::vector<double> coords){
     double x,y;
     x = coords[0];
     y = coords[1];
@@ -99,17 +138,80 @@ double get_magnitude(std::vector<float> coords){
     return sqrt(pow(x,2)+pow(y,2));
 }
 
-std::vector<float> get_all_data_magnitudes(std::vector<std::vector<float>> data){
+
+std::vector<double> get_all_data_magnitudes(std::vector<std::vector<double>> data){
     
-    std::cout << "Magnitudes: " << std::endl << std::endl;
-    std::vector<float> all_magnitudes;
+    std::cout << std::endl << "Magnitudes: " << std::endl << std::endl;
+    std::vector<double> all_magnitudes;
     for (int i=0; i<data.size(); ++i){
 
-        std::vector<float> coords = data[i];
+        std::vector<double> coords = data[i];
         double magnitude = get_magnitude(coords);
         all_magnitudes.push_back(magnitude);
-        std::cout << magnitude << std::endl;
     }
 
+    print_result(all_magnitudes);
+
     return all_magnitudes;
+}
+
+
+double fit_function(double x, double p, double q){
+    return p*x +q;
+}
+
+
+double get_chi_sqrd(std::vector<std::vector<double>> data, double p, double q){
+
+    std::vector<std::vector<double>> err_data = read_data("error2D_float.txt");
+}
+
+
+std::vector<double> least_squares_linear_fit(std::vector<std::vector<double>> data){
+
+    // extract x and y data into separate arrays
+    double x_sum, y_sum, x_sq_sum, xy_sum = 0;
+
+    for (int i=0; i < data.size(); ++i){
+        x_sum += data[i][0];
+        x_sq_sum += pow(data[i][0],2);
+        y_sum += data[i][1];
+        xy_sum += data[i][0]*data[i][1];
+    }
+
+    double N = data.size();
+
+    double ls_denominator = N*x_sq_sum-x_sum*x_sum;
+    double p = (N*xy_sum - x_sum*y_sum)/ls_denominator;
+    double q = (x_sq_sum*y_sum - xy_sum*x_sum)/ls_denominator;
+
+    // this is how I interpreted instruction 7 in the assignment, literally just print the linear equation as a string
+    std::string fit_line_equation = "y = " + std::to_string(p) + "x + " + std::to_string(q);
+
+    // **save output to .txt file**
+
+    std::ofstream linearFitOuput;
+    // open file
+    linearFitOuput.open("linearFitFunction.txt");
+    // check
+    if (linearFitOuput.fail()){
+        print_result("Failed to open linear fit output file");
+        exit(1);
+    }
+
+    // write in data
+    linearFitOuput << fit_line_equation << std::endl;
+
+    // close file
+    linearFitOuput.close();
+
+    //print output to terminal
+    print_result("Fitted function:");
+    print_result(fit_line_equation);
+
+    std::vector<double> lin_params;
+    lin_params.push_back(p);
+    lin_params.push_back(q);
+
+    return lin_params;
 }
--- Exercises2023/Ex1_2/CustomFunctions.h
+++ Exercises2023/Ex1_2/CustomFunctions.h
@@ -4,17 +4,26 @@
 
 #pragma once
 
+// overloaded functions to print various objects
+void print_result(std::string result);
+void print_result(double result);
+void print_result(std::vector<double> result);
+
 // function to read coordinate data CSV file and output data as a vector of vectors
 // containing corresponding pairs of x and y values
-std::vector<std::vector<float>> read_data(std::string filename);
+std::vector<std::vector<double>> read_data(std::string filename);
 
 // function to print as many lines of the data stored in the input vector of vectors
 // as the user requests up to the maximum
-void print_data(std::vector<std::vector<float>> data);
+void print_data(std::vector<std::vector<double>> data);
 
 // function to output the magnitude of the inputted 2D vector
-double get_magnitude(std::vector<float> coords);
+double get_magnitude(std::vector<double> coords);
 
 // function to print and store all the magnitudes of the 2D vectors in the input
 // vector
-std::vector<float> get_all_data_magnitudes(std::vector<std::vector<float>> data);
+std::vector<double> get_all_data_magnitudes(std::vector<std::vector<double>> data);
+
+// function to produce a linear fit of the x,y data using the least squares method
+std::vector<double> least_squares_linear_fit(std::vector<std::vector<double>> data);
+
